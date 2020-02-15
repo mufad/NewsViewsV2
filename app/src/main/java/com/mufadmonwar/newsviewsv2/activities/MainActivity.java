ea0524a2b77e24a8ebe71fd81f30fb18c6001915
Mufad Monwar <mmufad@gmail.com>
2020-02-15 13:16:08 +0600

    drawer added to MainActivity

--- app/src/main/java/com/mufadmonwar/newsviewsv2/activities/MainActivity.java
+++ app/src/main/java/com/mufadmonwar/newsviewsv2/activities/MainActivity.java
@@ -1,16 +1,80 @@
 package com.mufadmonwar.newsviewsv2.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
 import com.mufadmonwar.newsviewsv2.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
+    private Toolbar toolbar;
+
+    private Activity mActivity;
+    private Context mContext;
+    private View navHeaderView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initVars();
+        initViews();
+    }
+    private void initVars() {
+        mActivity = MainActivity.this;
+        mContext = mActivity.getApplicationContext();
+    }
+
+    private void initViews() {
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout_lawyer);
+        navigationView = findViewById(R.id.navigation_view);
+        initToolbar();
+        enableBackButton();
+
+        setToolbarTitle(getString(R.string.app_name));
+        initDrawer();
+    }
+
+    private void initDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(mActivity, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navHeaderView = navigationView.getHeaderView(0);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        if (drawerLayout != null && drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
